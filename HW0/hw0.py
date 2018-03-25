0d3f4a26ff74c2ff16b2cfae90b1d7e45c52ae3b
shimonarz <shimon1171@gmail.com>
2018-03-25 22:36:59 +0300

    Changes after connect to server

--- HW0/hw0.py
+++ HW0/hw0.py
@@ -1,19 +1,8 @@
 import csv
 
-Graph = {}
-
-########################################################################################
-###################  csv files to dictionaries #########################################
-with open("graph.csv","r") as csvfile:
-    reader = csv.DictReader(csvfile)
-    # note : field_names = ['Node 1', 'Node 2','W or S' ]
-    for row in reader:
-        Graph[(row['Node 1'],row['Node 2'])] = row['W or S']
-csvfile.close()
-
-
 #######################################################################################
-############## Insert Your Answers ####################################################
+
+# <editor-fold desc="Our Code">
 
 strong_edge = "s"
 weak_edge = "w"
@@ -21,7 +10,7 @@ empyt_edge = ""
 not_exsist_edge = "null"
 
 def add_node_to_dic(dic, key_node , value_node):
-    if key_node not in nodes_dic:
+    if key_node not in dic:
         dic[key_node] = [value_node]
     else:
         dic[key_node].append(value_node)
@@ -66,52 +55,77 @@ def build_empyt_edges_weight_lists(empyt_edges_weight, nodes_dic,Graph):
                     elif x_a_edge_weight == empyt_edge and x_b_edge_weight == strong_edge:
                         empyt_edges_weight[x_a_edge].append(strong_edge)
 
+# </editor-fold>
 
-# for ech node we bulid list of all connected nodes
-nodes_dic = {}
-for key in Graph:
-    a = key[0]
-    b = key[1]
-    add_node_to_dic(nodes_dic,a,b)
-    add_node_to_dic(nodes_dic,b,a)
+#######################################################################################
 
-# for eche empty edge we bulid list of all optinal weight option
-empyt_edges_weight = {}
-for nodes_tuple in Graph:
-    edge_weight = Graph[nodes_tuple]
-    if edge_weight == empyt_edge:
-        empyt_edges_weight[nodes_tuple] = []
 
-build_empyt_edges_weight_lists(empyt_edges_weight, nodes_dic,Graph)
+def read_file(graph_file_path):
+    """reading csv file to a dictionary"""
+    graph = {}
+    with open(graph_file_path, "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        # note : field_names = ['Node 1', 'Node 2','W or S' ]
+        for row in reader:
+            graph[(row['Node 1'], row['Node 2'])] = row['W or S']
+    csvfile.close()
+    return graph
 
-# we choose for eche empty edge tha maximum value of weight that it can get : max is strong.
-for nodes_tuple in empyt_edges_weight:
-    weights = empyt_edges_weight[nodes_tuple]
-    if strong_edge in weights:
-        Graph[nodes_tuple] = strong_edge
+
+def write_file(graph, id1, id2=None):
+    """writing dictionary to a csv files"""
+    if id2 is None:
+        filename = 'hw0_{}.csv'.format(id1)
     else:
-        Graph[nodes_tuple] = weak_edge
+        filename = 'hw0_{}_{}.csv'.format(id1, id2)
+    with open(filename, 'w') as csv_file:
+        writer = csv.writer(csv_file, lineterminator='\n')
+        fieldnames2 = ["Node 1", "Node 2", "W or S"]
+        writer.writerow(fieldnames2)
+        for edge in graph:
+            writer.writerow([edge[0], edge[1], graph[edge]])
 
 
+def main():
+    graph_file_path = './graph.csv'
+    graph = read_file(graph_file_path)
 
-#######################################################################################
-###################### End Answers ####################################################
+    ID1 = "037036209"
+    ID2 = "xxxxxxxxx" # TODO: Insert your ID's
+
+    #######################################################################################
+    # for ech node we bulid list of all connected nodes
+    nodes_dic = {}
+    for key in graph:
+        a = key[0]
+        b = key[1]
+        add_node_to_dic(nodes_dic, a, b)
+        add_node_to_dic(nodes_dic, b, a)
+
+    # for eche empty edge we bulid list of all optinal weight option
+    empyt_edges_weight = {}
+    for nodes_tuple in graph:
+        edge_weight = graph[nodes_tuple]
+        if edge_weight == empyt_edge:
+            empyt_edges_weight[nodes_tuple] = []
+
+    build_empyt_edges_weight_lists(empyt_edges_weight, nodes_dic, graph)
+
+    # we choose for eche empty edge tha maximum value of weight that it can get : max is strong.
+    for nodes_tuple in empyt_edges_weight:
+        weights = empyt_edges_weight[nodes_tuple]
+        if strong_edge in weights:
+            graph[nodes_tuple] = strong_edge
+        else:
+            graph[nodes_tuple] = weak_edge
+
+    #######################################################################################
+
+    write_file(graph, ID1, ID2)
 
 
-
-
-########################################################################################
-#######  output file ###################################################################        
-with open('HW0_037036209_XXXXXXXXX.csv', 'w' ) as write_file:
-    writer = csv.writer(write_file, lineterminator='\n')
-    fieldnames2 = ["Node 1" , "Node 2" ,"W or S"]
-    writer.writerow(fieldnames2)
-    for arc in Graph:
-        writer.writerow([  arc[0] , arc[1] , Graph[arc]  ])
-write_file.close
-#######################################################################################
-#######################################################################################
-
+if __name__ == '__main__':
+    main()
 
 
 
